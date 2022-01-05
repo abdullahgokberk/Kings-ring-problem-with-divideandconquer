8efe9627b984bf6ed6f1162b83179a5e881265ea
abdullahgokberk <96878770+abdullahgokberk@users.noreply.github.com>
2022-01-05 21:09:10 +0300

    Add files via upload

---
+++ Mat333_kings_ring_problem.py
@@ -0,0 +1,56 @@
+import random
+def divide(rings):
+    #Divide
+    if len(rings)>1:
+        mid= len(rings) // 2
+        left=rings[:mid]
+        right=rings[mid:]
+        divide(left)
+        divide(right)
+        conquer(rings, left, right)
+def conquer(rings,left,right):
+    #conquer
+    a=0
+    b=0
+    c=0
+    while a<len(left) and b<len(right):
+        if left[a]<right[b]:
+            rings[c]=left[a]
+            a=a+1
+        else:
+            rings[c]=right[b]
+            b=b+1
+        c=c+1
+    while a<len(left):
+        rings[c]=left[a]
+        a=a+1
+        c=c+1
+    while b<len(right):
+        rings[c]=right[b]
+        b=b+1
+        c=c+1    
+        
+# array initialization
+rings = [];
+for x in range(1000):
+    rings.append(10)
+i=random.randint(1,1000);
+rings[i]=11;
+divide(rings)
+print(rings)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
